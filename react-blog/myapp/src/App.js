f1e8b9441dec791ca9e4358873a423bf4c9b3809
jihyeony <jihyeon.kim@hh9works.com>
2022-06-08 18:54:41 +0900

    feat : 데이터바인딩, JSX문법, 버튼 기능개발, state

--- react-blog/myapp/src/App.js
+++ react-blog/myapp/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
 
 function App() {
+  let title = '글목록';
+  let [글제목, 글제목변경]= useState(['남자코트추천', '강남우동맛집', '파이썬독학']);
+  let [따봉, 따봉변경] = useState(0);
+
+  function test(){
+    console.log(1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <h1>{title}</h1>
+      </div>
+      <div className="content">
+        <div className="list">
+        <div>
+          <button onClick={ ()=>{
+            let copy = [...글제목];
+            copy[0] = '여자코트추천';
+            글제목변경(copy);
+          } 
+        } type='button'>제목을바꾸는버튼</button>
+        </div>
+          <h2>{글제목[0]} <span onClick={ ()=>{따봉변경(따봉 + 1)} }>좋아요</span> {따봉}</h2>
+          <p>2월 17일 발행</p>
+        </div>
+        <div className="list">
+        <h2>{글제목[1]}</h2>
+          <p>2월 17일 발행</p>
+        </div>  
+        <div className="list">
+        <h2>{글제목[2]}</h2>
+          <p>2월 17일 발행</p>
+        </div>
+      </div>
     </div>
   );
 }
