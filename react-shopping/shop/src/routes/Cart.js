90fec35fd04beafb5d8acaf658ff8c9c7144a335
jihyeony <jihyeon.kim@hh9works.com>
2022-06-22 18:27:49 +0900

    feat : rerdux를 사용한 함수생성

--- react-shopping/shop/src/routes/Cart.js
+++ react-shopping/shop/src/routes/Cart.js
@@ -1,13 +1,16 @@
 import {Table} from 'react-bootstrap'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import {changeName } from './../store'
 
 function Cart(){
 
-  let state = useSelector( (state) => { return state } )
-  console.log(state.stock);
+  let state = useSelector( (state) => { return state } );
+  let dispatch = useDispatch();
 
   return (
     <div>
+      {state.user}의 가방
+
       <Table>
       <thead>
         <tr>
@@ -18,12 +21,20 @@ function Cart(){
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>안녕</td>
-          <td>안녕</td>
-          <td>안녕</td>
-        </tr>
+        {
+          state.cart.map((a, i) =>
+            <tr key={i}>
+              <td>{state.cart[i].name}</td>
+              <td>{state.cart[i].count}</td>
+              <td>
+              <button onClick={ ()=>{
+                dispatch(changeName())
+              } }>
+              +
+              </button>  </td>
+            </tr>
+          )
+        }
       </tbody>
     </Table> 
     </div>
