dac84c18a8d7245863159630416f70fed82d3de1
jihyeon.kim <jihyeon.kim@hnine.com>
2022-10-14 18:46:41 +0900

    feat : 파일 업로드 및 3d모델 캔버스에 추가

--- react-ep1/my-three/src/App.js
+++ react-ep1/my-three/src/App.js
@@ -10,18 +10,52 @@ import state from "./components/state";
 
 // R3F
 import { Mesh } from 'three';
-import { Html, Preload, useProgress, useGLTFLoader } from "@react-three/drei";
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Html, Preload, useProgress,useGLTF } from "@react-three/drei";
+import { Canvas, useFrame, useThree, useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 // React Spring
 import { a, useTransition } from "@react-spring/web";
 //Intersection Observer
 import { useInView } from "react-intersection-observer";
 
+function Model() {
+  const gltf = useGLTF('/armchairYellow.gltf');
+  return (<primitive object={gltf.scene} />);
+}
+
+const Lights = () =>{
+  return(
+    <>
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[10,10,5]} intensity={1} />
+      <directionalLight position={[0,10,0]} intensity={1.5} />
+      <spotLight intensity={1} position={[1000, 1000, 0]} />
+    </>
+  )
+}
+
 const HTMLContent = () => {
+
+
+  {
+    /*
+      1. canvas를 추가하고 camera를 세팅한다
+      2. 3d모델을 불러온다
+      3. light를 원하는만큼 추가한다
+      4.원하는 동작을 추가한다.
+     */
+  }
+
+  const ref = useRef()
+  useFrame( () => (ref.current.rotation.y -= 0.01) );
+
   return (
     <Section factor={1.5} offset={1}>
       <group position={[0, 250, 0]}>
+      <mesh ref={ref} position={[0, -35, 0]}>
+        <Model />
+      </mesh>
         <Html fullscreen>
           <div className="container">
             <h1 className="title">Hello</h1>
@@ -36,12 +70,16 @@ function App() {
   return (
     <>
       <Header />
+      <Suspense fallback={null}>
       <Canvas
         colormanagement = "true"
         camera={{position:[0, 0, 120], fov:70}}
         >
+        <Lights />
         <HTMLContent />
+  
       </Canvas>
+      </Suspense>
     </>
   );
 }
