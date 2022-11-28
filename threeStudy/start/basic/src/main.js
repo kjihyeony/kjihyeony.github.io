648057d25c7b93bd93af68ef1bdbad52cb727a48
jihyeon.kim <jihyeon.kim@hnine.com>
2022-11-28 14:36:57 +0900

    feat : 배경색, 투명도 조절 설정

--- threeStudy/start/basic/src/main.js
+++ threeStudy/start/basic/src/main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { PerspectiveCamera } from 'three';
-import example from './ex02';
+// import example from './ex01';
+// import example from './ex02';
+import example from './ex03';
 
 example();
