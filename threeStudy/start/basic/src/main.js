c86e9f355f1698f966dfcd71e6d33a890cd9f202
jihyeon.kim <jihyeon.kim@hnine.com>
2022-11-28 17:18:28 +0900

    feat : three.js fog 사용해보기

--- threeStudy/start/basic/src/main.js
+++ threeStudy/start/basic/src/main.js
@@ -6,6 +6,7 @@ import { PerspectiveCamera } from 'three';
 // import example from './ex04';
 // import example from './ex05';
 // import example from './ex06';
-import example from './ex06-1';
+// import example from './ex06-1';
+import example from './ex7';
 
 example();
