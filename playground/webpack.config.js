12ccd33f850facda52fc53c4c197fa6235cd878e
jihyeon.kim <jihyeon.kim@hnine.com>
2022-12-14 18:28:48 +0900

    feat : htmlPlugin 추가

--- playground/webpack.config.js
+++ playground/webpack.config.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
+const pages = ["a", "b"];
+
 module.exports={
   //진입점
   entry: './src/index.js',
@@ -38,7 +40,12 @@ module.exports={
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './html/index.html'
+      template: './html/index.html',
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './html/contact.html',
+      filename: 'contact.html'
     }),
     new MiniCssExtractPlugin({
       filename: "common.css",
