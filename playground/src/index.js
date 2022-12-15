9ac7418a7aeb8acd4c05e21e303fd92ec1dd0022
jihyeon.kim <jihyeon.kim@hnine.com>
2022-12-15 18:04:20 +0900

    feat : 페이지간 트랜지션 추가

--- playground/src/index.js
+++ playground/src/index.js
@@ -6,11 +6,39 @@ import './css/style.scss'
 barba.use(barbaCss);
 
 
-
 //init Barba
 barba.init({
   transitions: [{
-    once() {},
-    leave() {} // enter or leave hook here
-  }]
+    name: 'main',
+    beforeOnce() {
+      console.log("before once");
+    },
+    once(){
+      /* with css plugin, this will not run*/
+      /* once hook only used for the css transition  */
+      console.log('once');
+    },
+    afterOnce() {
+      console.log('afterOnce');
+    } // enter or leave hook here
+  },{
+      name: 'fade',
+      to: {
+        namespace: ['fade']
+      },
+      leave() {},
+      //next page when entering when we are goimg fro, the first page to the second page
+      enter() {},
+    },
+    {
+      name: 'fade',
+      to: {
+        namespace: ['main']
+      },
+      leave() {},
+      //next page when entering when we are goimg fro, the first page to the second page
+      enter() {},
+    }
+
+  ]
 });
