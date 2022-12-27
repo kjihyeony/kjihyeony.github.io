041bccb6bc9c4606e8b12fa792e072465589f98f
jihyeon.kim <jihyeon.kim@hnine.com>
2022-12-27 10:13:48 +0900

    feat : ripple 테스트

--- playground/src/index.js
+++ playground/src/index.js
@@ -3,6 +3,11 @@ import barbaCss from '@barba/css';
 import gsap from 'gsap';
 import { Showcase } from "./js/Showcase"
 import { Slides } from './js/Slides';
+import { Curtains,
+  Plane,
+  Vec2,
+  PingPongPlane,t} from 'curtainsjs';
+import * as dat from 'dat.gui';
 import './css/style.scss'
 
 
@@ -94,6 +99,119 @@ if (bgAnimation) {
 
 }
 
+const rippleTest = document.getElementById('ripple');
+if(rippleTest) {
+  //  set up webGl contetxt or set up curtiansjs
+  const curtains = new Curtains({
+    container: "canvas",
+    pixelRatio: Math.min(1.5, window.devicePixelRatio),
+  });
+
+   // mouse/touch move
+   const ww = window.innerWidth;
+   const wh = window.innerHeight;
+
+   const mouse = new Vec2(ww / 5, wh / 8);
+   const lastMouse = mouse.clone();
+   const velocity = new Vec2();
+
+   function onMouseMove(e) {
+     lastMouse.copy(mouse);
+
+     // touch event
+     if (e.targetTouches) {
+       mouse.set(e.targetTouches[0].clientX, e.targetTouches[0].clientY);
+     } else {
+       mouse.set(e.clientX, e.clientY);
+     }
+
+     velocity.set((mouse.x - lastMouse.x) / 16, (mouse.y - lastMouse.y) / 16);
+
+     updateVelocity = true;
+   }
+
+   window.addEventListener("mousemove", onMouseMove);
+   window.addEventListener("touchmove", onMouseMove, {
+     passive: true,
+   });
+
+   const planeElement = document.getElementById("flowmap");
+
+   // parameters
+   const flowMapParams = {
+     sampler: "uFlowMap",
+     vertexShader: flowmapVs,
+     fragmentShader: flowmapFs,
+     texturesOptions: {
+       floatingPoint: "half-float",
+     },
+     uniforms: {
+       mousePosition: {
+         name: "uMousePosition",
+         type: "2f",
+         value: mouse,
+       },
+       fallOff: {
+         name: "uFalloff",
+         type: "1f",
+         value: ww > wh ? ww / 10000 : wh / 10000,
+ 
+         // we can change what i want here
+       },
+       cursorGrow: {
+         name: "uCursorGrow",
+         type: "1f",
+         value: 1.15,
+       },
+       // alpha of the cursor
+       alpha: {
+         name: "uAlpha",
+         type: "1f",
+         value: 1.14,
+       },
+       dissipation: {
+         name: "uDissipation",
+         type: "1f",
+         value: 0.925,
+       },
+       velocity: {
+         name: "uVelocity",
+         type: "2f",
+         value: velocity,
+       },
+       aspect: {
+         name: "uAspect",
+         type: "1f",
+         value: ww / wh,
+       },
+     },
+   };
+   const flowMap = new PingPongPlane(curtains, planeElement, flowMapParams);
+
+   flowMap.onRender(() => {
+     // update mouse position
+     flowMap.uniforms.mousePosition.value = flowMap.mouseToPlaneCoords(mouse);
+ 
+     flowMap.uniforms.velocity.value = new Vec2(
+       curtains.lerp(velocity.x, 0.5, 1.5),
+       curtains.lerp(velocity.y, 0.5, 1.5)
+     );
+   });
+   // add displacements shader
+   const params = {
+     vertexShader: displacementVs,
+     fragmentShader: displacementFs,
+   };
+   // create plane
+   const plane = new Plane(curtains, planeElement, params);
+ 
+   // create a texture that will hold our flowmap
+   const flowTexture = plane.createTexture({
+     sampler: "uFlowTexture",
+     fromTexture: flowMap.getTexture(), // set it based on our PingPongPlane flowmap plane's texture
+   });
+
+}
 
 
 
--- playground/src/js/shaders.js
+++ playground/src/js/shaders.js
@@ -66,61 +66,12 @@ void main(){
 `;
 
 const vertex = `
-#define PI 3.14159265359
-uniform float u_offset;
-uniform float u_progress;
-uniform float u_direction;
-uniform float u_time;
-uniform float u_waveIntensity;
 varying vec2 vUv;
+
 void main(){
-    vec3 pos = position.xyz;
-
-    float distance = length(uv.xy - 0.5 );
-    float sizeDist = length(vec2(0.5,0.5));
-    float normalizedDistance = distance/sizeDist ;
-
-    float stickOutEffect = normalizedDistance ;
-    float stickInEffect = -normalizedDistance ;
-
-
-    float stickEffect = mix(stickOutEffect,stickInEffect, u_direction);
-
-    // Backwards V wave.
-    float stick = 0.5;
-
-    float waveIn = u_progress*(1. / stick);
-    float waveOut =  -( u_progress - 1.) * (1./(1.-stick) );
-    waveOut = pow(smoothstep(0.,1.,waveOut),0.7);
-
-    float stickProgress = min(waveIn, waveOut);
-
-
-
-
-
-    // We can re-use stick Influcse because this oen starts at the same position
-    float offsetInProgress = clamp(waveIn,0.,1.);
-
-    // Invert stickout to get the slope moving upwards to the right
-    // and move it left by 1
-    float offsetOutProgress = clamp(1.-waveOut,0.,1.);
-
-    float offsetProgress = mix(offsetInProgress,offsetOutProgress,u_direction);
-
-
-    float stickOffset = u_offset;
-    pos.z += stickEffect * stickOffset * stickProgress  - u_offset * offsetProgress;
-
-
-    pos.z += sin(distance * 8. - u_time * 2. )  * u_waveIntensity;
-
-    gl_Position =
-        projectionMatrix *
-        modelViewMatrix *
-         vec4(pos, 1.0);
-
-    vUv = uv;
+  vec3 pos = position.xyz;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.);
+  vUv = uv;
 }
 `;
 
