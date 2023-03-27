a2b6b5b61404bb9cda1aaca37360b7976fd0a42c
jihyeon.kim <jihyeon.kim@hnine.com>
2023-03-27 11:30:08 +0900

    feat : 레퍼 구현

--- polio-parcel/polio/index.js
+++ polio-parcel/polio/index.js
@@ -5,34 +5,64 @@ import gsap from 'gsap';
  * 터스텀 커서
  */
 
-const figure = document.querySelector('.h9figureVideo');
-const mainCursor = (el) => {
-  const cursor = document.getElementById('cursor');
-  const sizer = document.querySelector('.sizer');
+// const figure = document.querySelector('.h9figureVideo');
+// const mainCursor = (el) => {
+//   const cursor = document.getElementById('cursor');
+//   const sizer = document.querySelector('.sizer');
 
 
-  el.addEventListener('mouseenter', (e) => {
-    sizer.classList.add('active');
-    const x = e.pageX - 10;
-    const y = e.pageY - 10;
+//   el.addEventListener('mouseenter', (e) => {
+//     sizer.classList.add('active');
+//     const x = e.pageX - 10;
+//     const y = e.pageY - 10;
 
-    cursor.style.left = x + "px";
-    cursor.style.top = y + "px";
+//     cursor.style.left = x + "px";
+//     cursor.style.top = y + "px";
+//   });
+
+//   el.addEventListener('mousemove', (e) => {
+//     const x = e.pageX - 10;
+//     const y = e.pageY - 10;
+
+//     cursor.style.left = x + "px";
+//     cursor.style.top = y + "px";
+
+//   });
+
+//   el.addEventListener('mouseleave', (e) => {
+//     sizer.classList.remove('active');
+//   });
+
+// };
+
+
+// mainCursor(figure);
+const gMove = () => {
+  const moveG = document.getElementById("moveG");
+
+  document.addEventListener("mousemove", function (e) {
+    const x = e.clientX;
+    const y = e.clientY;
+    moveG.setAttribute("transform", "translate(" + x + "," + y + ")");
+  });
+  
+}
+
+const playClick = () => {
+  const videoClip = document.querySelector('#video-clip');
+  const circle= document.querySelector('.video-clip .clippy circle')
+
+  videoClip.addEventListener('click', () => {
+    if (videoClip.classList.contains('video-playing')) {
+      videoClip.classList.remove('video-playing');
+      circle.style.transform = 'scale(3.2)';
+    } else {
+      videoClip.classList.add('video-playing');
+      circle.style.transform = 'scale(50)';
+    }
   });
 
-  el.addEventListener('mousemove', (e) => {
-    const x = e.pageX - 10;
-    const y = e.pageY - 10;
+}
 
-    cursor.style.left = x + "px";
-    cursor.style.top = y + "px";
-
-  });
-
-  el.addEventListener('mouseleave', (e) => {
-    sizer.classList.remove('active');
-  });
-};
-
-
-mainCursor(figure);
+gMove();
+playClick();
